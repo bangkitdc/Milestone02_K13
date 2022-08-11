ddb72234362895fb2acdfbb4bba1ae2509ac1688
bangkitdc <87227379+bangkitdc@users.noreply.github.com>
2022-08-12 02:44:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,16 +29,6 @@ const App = () => {
                         </Routes>
                     </Container>
                 </ThemeProvider>
-                <Container maxWidth="xl">
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Navigate replace to="/posts" />} />
-                        <Route path="/posts" exact element={<Home />}/>
-                        <Route path="/posts/search" exact element={<Home />}/>
-                        <Route path="/posts/:id" exact element={<PostDetails />}/>
-                        <Route path="/auth" exact element={(!user ? <Auth /> : <Navigate replace to="/posts" />)} />
-                    </Routes>
-                </Container>
             </BrowserRouter>
         </GoogleOAuthProvider>
     );
