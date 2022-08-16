8a7bef78551d05ee39fcf6439ca71b4782f95e72
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-08-16 17:47:24 +0700

    Minor Change

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import { Container, ThemeProvider } from '@material-ui/core';
+import React from "react";
+import { Container, ThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import PostDetails from './components/PostDetails/PostDetails';
-import Navbar from './components/NavBar/Navbar';
-import Home from './components/Home/Home';
-import Auth from './components/Auth/Auth';
-import Footer from './components/Footer/Footer';
+import PostDetails from "./components/PostDetails/PostDetails";
+import Navbar from "./components/NavBar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import Footer from "./components/Footer/Footer";
 
-import theme from './theme';
+import theme from "./theme";
 
 const App = () => {
-    const user = JSON.parse(localStorage.getItem('profile'));
+  const user = JSON.parse(localStorage.getItem("profile"));
 
-    return (
-        <GoogleOAuthProvider clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}>
-            <BrowserRouter>
-                <ThemeProvider theme={theme}>
-                    <Container maxWidth="xl">
-                        <Navbar />
-                        <Routes>
-                            <Route path="/" element={<Navigate replace to="/posts" />} />
-                            <Route path="/posts" exact element={<Home />}/>
-                            <Route path="/posts/search" exact element={<Home />}/>
-                            <Route path="/posts/:id" exact element={<PostDetails />}/>
-                            <Route path="/auth" exact element={(!user ? <Auth /> : <Navigate replace to="/posts" />)} />
-                        </Routes>
-                        <Footer />
-                    </Container>
-                </ThemeProvider>
-            </BrowserRouter>
-        </GoogleOAuthProvider>
-    );
-}
+  return (
+    <GoogleOAuthProvider
+      clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Container maxWidth="xl">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Navigate replace to="/posts" />} />
+              <Route path="/posts" exact element={<Home />} />
+              <Route path="/posts/search" exact element={<Home />} />
+              <Route path="/posts/:id" exact element={<PostDetails />} />
+              <Route
+                path="/auth"
+                exact
+                element={!user ? <Auth /> : <Navigate replace to="/posts" />}
+              />
+            </Routes>
+            <Footer />
+          </Container>
+        </ThemeProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  );
+};
 
 export default App;
